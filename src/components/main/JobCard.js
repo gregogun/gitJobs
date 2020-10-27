1f11d10e040cf73347ebdfff17a51787d3b75a10
Greg Ogun <gregogun97@gmail.com>
2020-10-27 10:27:45 +0000

    Delete unnecessary files

--- src/components/main/JobCard.js
+++ src/components/main/JobCard.js
@@ -1,16 +1,29 @@
 import React from "react";
-import './JobCard.css';
+import "./JobCard.css";
 
-const JobCard = ({ id, created, type, title, company, location }) => {
-    return (
-        <>
-            <p>{created}</p>
-            <p>{type}</p>
-            <p>{title}</p>
-            <p>{company}</p>
-            <p>{location}</p>
-        </>
-    )
-}
+const JobCard = ({
+  id,
+  created,
+  type,
+  title,
+  company,
+  location,
+  handleClick,
+}) => {
+  return (
+    <>
+      <p>{created}</p>
+      <p>{type}</p>
+      <p
+        onClick={handleClick}
+        style={{ color: "lightseagreen", cursor: "pointer" }}
+      >
+        {title}
+      </p>
+      <p>{company}</p>
+      <p>{location}</p>
+    </>
+  );
+};
 
 export default JobCard;
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,13 @@
 import React from "react";
 import JobBoard from "./JobBoard";
-import '../main/Main.css';
-
-
+import "../main/Main.css";
 
 const Main = () => {
-    return (
-        <div className="Main">
-        <JobBoard />
-        </div>
-    )
-}
+  return (
+    <div className="Main">
+      <JobBoard />
+    </div>
+  );
+};
 
 export default Main;
