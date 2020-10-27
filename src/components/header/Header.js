567dc679c6f8b275dfaf3aa6ded1cf543be59b97
Greg Ogun <gregogun97@gmail.com>
2020-10-27 21:10:37 +0000

    Create function to handle page refresh

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Header = () => {
+  const handleRefresh = () => {
+    window.location.reload(false);
+  };
+
   return (
     <div className="Header">
-      <h1>gitJobs</h1>
+      <h1 onClick={handleRefresh} style={{ cursor: "pointer" }}>
+        gitJobs
+      </h1>
     </div>
   );
 };
